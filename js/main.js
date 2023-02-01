6a42b14a78cf783c6d76236344c2c224204d8904
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-01-31 22:23:33 -0500

    [added] some changes

--- js/main.js
+++ js/main.js
@@ -183,7 +183,6 @@
 				$('.number').each(function(){
 					var $this = $(this),
 						num = $this.data('number');
-						console.log(num);
 					$this.animateNumber(
 					  {
 					    number: num,
